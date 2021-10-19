05be7e6ca6f8c26ca7b0a120d00248a3fce96c1d
Ivy Kathrina <88631468+newbiedev08@users.noreply.github.com>
2021-10-19 19:09:17 +0200

    Add files via upload

---
+++ opdracht2.js
@@ -0,0 +1,27 @@
+//using &&//
+var myAge= 13
+if (myAge > 18 &&  25){
+    console.log(" Je krijg 50% korting")
+} else{
+    console.log (" Sorry je krijgt geen korting");
+}
+
+//using || //
+const persoon = Sarah
+if ('firstName Sarah || Bram'){
+    console.log("Gefeliciteerd je krijgt een gratis biertje.");
+} else {
+    console.log("Helaas, geen gratis bier voor je");
+}
+
+//   // 
+const bestelKost = 100 ;
+if (totalAmount > 25)
+console.log('Alsjublieft, gratis (vega) bitterballen');
+  if ('totalAmount >50') 
+  console.log('Alsjublieft, een gratis portie Nachos');
+    if ('totalAmount >100') 
+    console.log('Alsjublieft, een gratis flesje champagne');
+  else {
+      console.log(' Helaas, geen gratis.')
+  }
